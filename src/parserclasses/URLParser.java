fb8ee41a2e68eac0087ed5d3e51f48d8f5996bf2
hemp85 <hemp85@hemp85-Latitude-E6410>
2016-04-05 08:52:25 +0200

    separate folders for vols and rr

--- src/parserclasses/URLParser.java
+++ src/parserclasses/URLParser.java
@@ -10,11 +10,11 @@ import org.jsoup.select.Elements;
 public class URLParser {
 		
 	public void checkData(Elements elems, String path) throws IOException{
-		
+		//checks and formats date
 		SimpleDateFormat std = new SimpleDateFormat("dd-MM-yyyy");
 		Date date = new Date();
 		String stringdate = std.format(date);
-		
+		//prepare files names
 		String[] vols = {stringdate+"\nATM VOLATILITIES.xml",stringdate+ "\n25-DELTA RISK REVERSAL.xml"};
 		File[] files = new File[2];
 		Boolean exist = false;
@@ -23,8 +23,17 @@ public class URLParser {
 		
 		
 		for(int i =0; i <files.length;i++){
-		files[i] = new File(path+"/"+vols[i]);	
-        exist = files[i].createNewFile();
+			if(vols[i].contains("VOLATILITIES")){
+				files[i] = new File(path+"/ATM VOLATILITIES");	
+				files[i].mkdirs();
+				files[i] = new File(path+"/ATM VOLATILITIES/"+vols[i]);
+				exist = files[i].createNewFile(); // returns true when creates file
+				continue;
+			}
+			files[i] = new File(path+"/25-DELTA RISK REVERSAL");	
+			files[i].mkdirs();
+			files[i] = new File(path+"/25-DELTA RISK REVERSAL/"+vols[i]);
+			exist = files[i].createNewFile(); // returns true when creates file
         }
 				
 		switch(exist.toString()){
@@ -35,12 +44,12 @@ public class URLParser {
 			
 			case "true" : 
 				stringElems = elems.toString();
-				tabs = stringElems.split("</table>");
+				tabs = stringElems.split("</table>");//clean and preapare data to save
 				StringBuilder str = new StringBuilder(tabs[0]+"</table>");
 				tabs[0] = str.toString();
 			break;
 		}
-		
+		//writing data in file
 		for(int i=0;i<tabs.length;i++){
 			FileWriter fw = new FileWriter(files[i]);
 			fw.write(tabs[i]);
